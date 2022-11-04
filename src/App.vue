4bc3f21b9d6cb19ce40a4fbbbeabbe5eaf95d5f1
Mihray <2636853047@qq.com>
2022-11-04 16:32:02 +0800

    fix：创建新项目

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <FirstModule></FirstModule>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FirstModule from './components/FirstModule.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FirstModule
   }
 }
 </script>
---
+++ src/components/FirstModule.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="square">
+
+    </div>
+</template>
+<script>
+
+</script>
+<style scoped>
+.square{
+    width:90vmin;
+    height:90vmin;
+    background-color: #369;
+}
+</style>
