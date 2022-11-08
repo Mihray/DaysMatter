7212ff8d04c30b095393c3ea50bf175f8690eb34
Mihray <2636853047@qq.com>
2022-11-09 00:35:52 +0800

    fix:创建了vue项目，添加了顶部小菜单图标

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
 <template>
-  <FirstModule></FirstModule>
+  <HomeModule></HomeModule>
 </template>
 
 <script>
-import FirstModule from './components/FirstModule.vue'
+import HomeModule from './components/HomeModule.vue'
 
 export default {
   name: 'App',
   components: {
-    FirstModule
+    HomeModule
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e50; */
+  /* margin-top: 60px; */
+  font-size: 14px;
+}
+body{
+  background-color: rgb(131, 222, 225);
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/HomeModule.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="square">
+        <div class="menu-line">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</template>
+<script>
+
+</script>
+<style scoped>
+.square{
+    /* width:100vmin;
+    height:15vmax; */
+    width:100vw;
+    height:10vh;
+    background-color: rgb(53, 162, 189);
+    position: absolute;
+}
+.menu-line{
+    width: 4vh;
+    height:4vh ;
+    /* background-color: brown; */
+    margin-top:4vh;
+    margin-left: 3vh;
+    /* box-sizing: border-box; */
+    display: flex;
+    flex-direction: column;
+    /* align-content:space-between; */
+    justify-content:space-between;
+}
+.menu-line div{
+    /* box-sizing: inherit; */
+    width: 4vh;
+    height:0.9vh ;
+    background-color: white;
+    /* align-content:space-between; */
+}
+/* .menu-line div:first-child{
+    align-items:flex-start;
+}
+.menu-line div:last-child{
+    align-items:flex-end;
+} */
+</style>
